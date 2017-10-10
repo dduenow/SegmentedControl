4cfe89d93093c2df478d06f18e6544596bd5126d
dduenow <davidduenow@mwc-120082.dhcp.missouri.edu>
2017-10-10 12:43:05 -0500

    Initial Commit

--- Segmented Control/ViewController.swift
+++ Segmented Control/ViewController.swift
@@ -10,9 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var segmentedControler: UISegmentedControl!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.view.backgroundColor = UIColor.red
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +23,21 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func segmentedAction(_ sender: AnyObject) {
+        let index = sender.selectedSegmentIndex
+        
+        switch index {
+        case 0?:
+            self.view.backgroundColor = UIColor.red
+        case 1?:
+            self.view.backgroundColor = UIColor.blue
+        case 2?:
+            self.view.backgroundColor = UIColor.green
+        default:
+            self.view.backgroundColor = UIColor.red
+        }
+        
+    }
+    
 }
 
